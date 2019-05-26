e5597d36956f14c03c1f51c51ab934761836477c
Toua Thao <touathao09@gmail.com>
2019-05-26 13:16:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Vue Ninja',
+        wage: 10,
+        name: 'Toua',
+        url: 'https://www.youtube.com/',
+        classes: ['one', 'two'],
+        coords:{
+            x:0,
+            y:0,
+        }
+    },
+    methods: {
+        greet(time) {
+            return `Hello and good ${time}, ${this.name}`
+        },
+        changeWage(amount) {
+            this.wage += amount
+        },
+        logEvent(event) {
+            console.log(event)
+        },
+        logCoords(event){
+            this.coords.x = event.offsetX
+            this.coords.y = event.offsetY
+        }
+    }
+})
